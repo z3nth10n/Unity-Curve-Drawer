13bb37b73b39128af4f658985315ad8f3191725f
Ikillnukes <ikillnukes1@gmail.com>
2018-11-14 12:21:33 +0100

    Initial commit

---
+++ Assets/Scripts/CurveDisplay.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveDisplay : MonoBehaviour
+{
+    public int width = 300;
+    public int height = 50;
+    public int seed = 12;
+
+    public float amplitude = .2f,
+                 meanHeight = -.25f;
+
+    private Texture2D texture;
+    private FastNoise noise;
+
+    private float lastAmplitude, lastMeanHeight;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        noise = new FastNoise();
+        noise.SetNoiseType(FastNoise.NoiseType.PerlinFractal);
+
+        noise.SetSeed(seed);
+        noise.SetFrequency(.01f);
+        noise.SetFractalOctaves(3);
+        noise.SetFractalLacunarity(1.25f);
+        noise.SetFractalGain(.5f);
+
+        ClearTexture();
+
+        UpdateMeanLine();
+
+        UpdateNoise();
+    }
+
+    private void ClearTexture()
+    {
+        texture = new Color(0, 0, 0, 0).ToTexture(width, height);
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                float y = (j / 2f) * height;
+
+                texture.SetPixel(i, (int)Mathf.Clamp(y, 10, height - 1), Color.gray);
+            }
+        }
+
+        texture.Apply();
+    }
+
+    private void UpdateMeanLine()
+    {
+        ClearTexture();
+
+        for (int m = 0; m < width; m++)
+        {
+            texture.SetPixel(m, height / 2, Color.yellow);
+        }
+
+        texture.Apply();
+    }
+
+    private void UpdateNoise(bool clear = false)
+    {
+        if (clear)
+            UpdateMeanLine();
+
+        for (int k = 0; k < width; k++)
+        {
+            float v = F.ConvertRange(-1, 1, 0, 1, noise.GetValueFractal(k, 0));
+
+            v = F.GetBoundedNoise(v, meanHeight, amplitude);
+
+            texture.SetPixel(k, (int)(v * (height - 1)), Color.green);
+        }
+
+        texture.Apply();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnGUI()
+    {
+        if (texture != null)
+            GUI.DrawTexture(new Rect(5, 5, width, height), texture);
+
+        GUILayout.BeginArea(new Rect(5, height + 10, width, 200));
+
+        GUILayout.Label($"Mean Height ({meanHeight.ToString("F2")})");
+
+        meanHeight = GUILayout.HorizontalSlider(meanHeight, -.5f, .5f);
+
+        GUILayout.Label($"Amplitude ({amplitude.ToString("F2")})");
+
+        amplitude = GUILayout.HorizontalSlider(amplitude, 0, 1);
+
+        GUILayout.EndArea();
+
+        if (meanHeight != lastMeanHeight)
+        {
+            UpdateMeanLine();
+            UpdateNoise();
+        }
+
+        if (amplitude != lastAmplitude)
+            UpdateNoise(true);
+
+        lastMeanHeight = meanHeight;
+        lastAmplitude = amplitude;
+    }
+}
---
+++ Assets/Scripts/F.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class F
+{
+    private static Dictionary<Color, Texture2D> dictionary = new Dictionary<Color, Texture2D>();
+
+    /// <summary>
+    /// To the texture.
+    /// </summary>
+    /// <param name="color">The color.</param>
+    /// <param name="width">The width.</param>
+    /// <param name="height">The height.</param>
+    /// <param name="store">if set to <c>true</c> [store].</param>
+    /// <returns></returns>
+    public static Texture2D ToTexture(this Color color, int width = -1, int height = -1, bool store = false)
+    {
+        Texture2D texture = null;
+
+        if (!dictionary.ContainsKey(color) || !store)
+        {
+            if (width == -1 || height == -1)
+            {
+                texture = new Texture2D(1, 1);
+                texture.SetPixel(0, 0, color);
+            }
+            else
+            {
+                texture = new Texture2D(width, height);
+
+                for (int i = 0; i < width * height; ++i)
+                    texture.SetPixel(i % width, i / width, color);
+            }
+
+            texture.Apply();
+
+            if (store) dictionary.Add(color, texture);
+        }
+
+        if (store)
+            return dictionary[color];
+        else
+            return texture;
+    }
+
+    /// <summary>
+    /// Converts the range.
+    /// </summary>
+    /// <param name="originalStart">The original start.</param>
+    /// <param name="originalEnd">The original end.</param>
+    /// <param name="newStart">The new start.</param>
+    /// <param name="newEnd">The new end.</param>
+    /// <param name="value">The value.</param>
+    /// <returns></returns>
+    public static float ConvertRange(
+        float originalStart, float originalEnd, // original range
+        float newStart, float newEnd, // desired range
+        float value) // value to convert
+    {
+        float scale = (newEnd - newStart) / (originalEnd - originalStart);
+        return (newStart + ((value - originalStart) * scale));
+    }
+
+    /// <summary>
+    /// Gets the bounded noise.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="meanHeight">Height of the mean.</param>
+    /// <param name="amplitude">The amplitude.</param>
+    /// <returns></returns>
+    public static float GetBoundedNoise(float value, float meanHeight, float amplitude)
+    {
+        //if (value < 0)
+        //    value = Mathf.Abs(value);
+
+        return Mathf.Clamp01(Mathf.InverseLerp(amplitude, 1f - amplitude, value) + (meanHeight + .5f));
+    }
+}
